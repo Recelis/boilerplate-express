283e880bf131f01d48b968c2877b67389b6b1e0c
Jacky Lui <jackymlui@live.com>
2018-12-05 11:05:17 +1000

    up to part 4 completed

--- myApp.js
+++ myApp.js
@@ -9,16 +9,21 @@ var app = express();
 
 
 /** 1) Meet the node console. */
-
+console.log("Hello World");
 
 /** 2) A first working Express Server */
-
+// app.get('/', function(req, res){
+//     res.send("Hello Express"); // sends strings out
+// });
 
 /** 3) Serve an HTML file */
-
+app.get('/', function(req, res){
+    let absolutePath = __dirname + "/views/index.html";
+    res.sendFile(absolutePath);
+});
 
 /** 4) Serve static assets  */
-
+app.use('/', express.static(__dirname+"/public"));
 
 /** 5) serve JSON on a specific route */
 
