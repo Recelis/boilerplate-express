87b826b07a1e590681b82a0e84c66df26cf2cc9f
Jacky Lui <jackymlui@live.com>
2018-12-07 18:35:00 +1000

    completed FCC Express section

--- myApp.js
+++ myApp.js
@@ -1,13 +1,17 @@
-require('dotenv').config();
+// require('dotenv').config();
+var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 // https://www.npmjs.com/package/dotenv
 
 // --> 7)  Mount the Logger middleware here
-
+app.use(function(req,res,next){
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+});
 
 // --> 11)  Mount the body-parser middleware  here
-
+app.use(bodyParser.urlencoded({extended: false}));
 
 /** 1) Meet the node console. */
 console.log("Hello World");
@@ -44,21 +48,34 @@ app.get('/json', function(req,res){
 
 
 /** 8) Chaining middleware. A Time server */
-
+app.get('/now', function(req,res,next){
+    req.time = new Date().toString();
+    next();
+}, function(req,res){
+    res.json({time:req.time});
+});
 
 /** 9)  Get input from client - Route parameters */
-
+app.get('/:word/echo', function(req,res){
+    res.json({echo:req.params.word});
+});
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
-
+// app.get('/name', function(req,res){
+//     res.json({name:req.query.first+' ' + req.query.last})
+// });
   
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
 
 
 /** 12) Get data form POST  */
-
+app.route('/name').get(function(req,res){
+    res.json({name:req.query.first+' ' + req.query.last})
+}).post(function(req,res){
+    res.json({name:req.body.first + ' ' + req.body.last})
+});
 
 
 // This would be part of the basic setup of an Express app
